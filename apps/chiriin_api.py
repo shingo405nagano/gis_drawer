7df9393de6c3a60dc2c952f4c2617cbe09c47d8b
shingo405nagano <makishin.nagano@gmail.com>
2024-02-15 22:33:12 +0900

    update: コメント追加

--- apps/chiriin_api.py
+++ apps/chiriin_api.py
@@ -1,8 +1,10 @@
 """
 国土地理院の提供しているAPIで情報を取得する為のモジュール
 https://vldb.gsi.go.jp/sokuchi/surveycalc/api_help.html
-地理院の用意しているAPIでは秒1回のリクエストに制限されている為
-一度取得するたびに1秒休ませている
+1. lonlat_to_altitude
+    経緯度から標高を取得する
+2. semidynamic_exe
+    セミダイナミック補正
 """
 from dataclasses import dataclass
 import logging
@@ -37,7 +39,7 @@ def lonlat_to_altitude(lon: float, lat: float, time_out: int=20) -> float:
     Doctest:
         >>> lonlat_to_altitude(141.307135647, 41.142745499)
         Altitude(altitude=108, source='10m')
-        
+
     """
     t = time.time()
     dummy = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
