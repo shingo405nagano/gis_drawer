4101aae74ed614a35c0aae727d6b21f50b4a1919
shingo405nagano <makishin.nagano@gmail.com>
2024-02-18 23:03:53 +0900

    fix: 座標の計算を修正

--- apps/spatial_materials.py
+++ apps/spatial_materials.py
@@ -9,12 +9,16 @@
 4. regular_hexagon_gdf
     指定範囲全体に指定した面積の正六角形のPolygonオブジェクトを作成する。
 """
+from copy import deepcopy
 from dataclasses import dataclass
 import math
 from typing import List
 
 import geopandas as gpd
 import shapely
+
+from apps.disassembly import geom_disassembly
+
 
 
 def _to_point(base_point, distance, angle) -> shapely.Point:
@@ -114,7 +118,7 @@ class Hexagons(object):
         """中心座標から設定している面積の六角形を作成する。"""
         center = shapely.Point(center_x, center_y)
         # 正六角形の一辺の長さを計算
-        side_length = math.sqrt(self._square_metre / (1.5 * math.sqrt(3)))
+        side_length = round(math.sqrt(self._square_metre / (1.5 * math.sqrt(3))), 4)
         # 正六角形の頂点座標を計算（時計回りで）
         h_vertices = []
         for angle in range(0, 420, 60):
@@ -136,6 +140,13 @@ class Hexagons(object):
         y_min = min([coord[1] for coord in poly.exterior.coords])
         return y_min
     
+    def _sbs_x_off(self, poly: shapely.Polygon) -> float:
+        coords = list(poly.exterior.coords)
+        right = coords[1][0]
+        left = coords[-2][0]
+        x_off = abs(right - left)
+        return x_off
+    
     def _side_by_side(
         self, 
         start_x: float, 
@@ -151,18 +162,30 @@ class Hexagons(object):
         Returns:
             List(shapely.Polygon): 横並びの六角形
         """
-        _xoff = math.sqrt(self._square_metre / (2 * math.sqrt(3))) * 2
-        xoff = _xoff - _xoff / 100_000_000
         hexagon = self._regular_hexagon(start_x, start_y)
-        hexagon_list = [hexagon]
+        x_off = self._sbs_x_off(hexagon)
+        hexagons = [hexagon]
         while True:
-            shift_hexagon = shapely.affinity.translate(hexagon_list[-1], xoff)
+            shift_hexagon = shapely.affinity.translate(hexagons[-1], x_off)
             x_max = self.__calc_x_max(shift_hexagon)
-            hexagon_list.append(shift_hexagon)
+            hexagons.append(shift_hexagon)
             if end_x < x_max:
                 break
-        return hexagon_list
+        return hexagons
     
+    def _lsbs_x_off(self, poly: shapely.Polygon) -> float:
+        center = poly.centroid
+        points = geom_disassembly(poly, 'point')
+        trg = shapely.MultiPoint(points[: 2]).centroid
+        x_off = center.distance(trg)
+        return x_off
+    
+    def _lsbs_y_off(self, poly: shapely.Polygon) -> float:
+        p1, p2 = geom_disassembly(poly, 'point')[: 2]
+        base = p1.distance(p2)
+        y_off = base / 2 + base * -1
+        return y_off
+        
     def _lower_side_by_side(
         self, 
         start_x: float, 
@@ -179,34 +202,87 @@ class Hexagons(object):
         Returns:
             List(shapely.Polygon): 横並びのずらした六角形
         """
-        _xoff = math.sqrt(self._square_metre / (2 * math.sqrt(3)))
-        xoff = _xoff - _xoff / 100_000_000
-        yoff = math.sqrt((xoff * 2) ** 2 - (xoff ** 2)) * -1
         hexagons = self._side_by_side(start_x, start_y, end_x)
+        x_off = self._lsbs_x_off(hexagons[0])
+        y_off = self._lsbs_y_off(hexagons[0])
         new_hexagons = []
         for hexagon in hexagons:
-            new_hexagon = shapely.affinity.translate(hexagon, xoff, yoff)
+            new_hexagon = shapely.affinity.translate(hexagon, x_off, y_off)
             new_hexagons.append(new_hexagon)
         # 右側にずらしたので、最初に新しく六角形を追加する。
-        first_hexagon = shapely.affinity.translate(new_hexagons[0], xoff * 2 * -1)
-        return [first_hexagon] + new_hexagons
+        x_off = self._sbs_x_off(hexagons[0]) * -1
+        first_hexagon = shapely.affinity.translate(new_hexagons[0], xoff=x_off)
+        new_hexagons = [first_hexagon] + new_hexagons
+        return new_hexagons
     
+    def __move_down_y_off(self, poly: shapely.Polygon) -> float:
+        point = geom_disassembly(poly, 'point')[0]
+        center = poly.centroid
+        return point.distance(center) * 3 * -1
+
     def __move_down(
         self, 
-        hexagons: List[shapely.Polygon], 
-        yoff: float
+        hexagons: List[shapely.Polygon],
+        y_off: float
     ) -> List[shapely.Polygon]:
         """下にずらす"""
-        new_hexagon_list = []
+        new_hexagons = []
         for hexagon in hexagons:
-            new_hexagon = shapely.affinity.translate(hexagon, yoff=yoff)
-            new_hexagon_list.append(new_hexagon)
-        return new_hexagon_list
+            new_hexg = shapely.affinity.translate(hexagon, yoff=y_off)
+            new_hexagons.append(new_hexg)
+        return new_hexagons
+    
+    def _fit(
+        self,
+        base_hexagons: List[shapely.Polygon], 
+        shift_hexagons: List[shapely.Polygon]
+    ) -> List[shapely.Polygon]:
+        base_hexagons = deepcopy(base_hexagons)
+        shift_hexagons = deepcopy(shift_hexagons)
+        if len(base_hexagons) <= len(shift_hexagons):
+            base_corner = 4
+        else:
+            base_corner = 2
+        result_hexagons = []
+        while True:
+            if (1 <= len(shift_hexagons)) & (1 <= len(base_hexagons)):
+                b_hexg = base_hexagons.pop(0)
+                s_hexg = shift_hexagons.pop(0)
+                base_pt = geom_disassembly(b_hexg, 'point')[base_corner]
+                shift_pt = geom_disassembly(s_hexg, 'point')[0]
+            elif 1 <= len(shift_hexagons):
+                b_hexg = result_hexagons[-1]
+                s_hexg = shift_hexagons.pop(0)
+                base_pt = geom_disassembly(b_hexg, 'point')[1]
+                shift_pt = geom_disassembly(s_hexg, 'point')[5]
+            else:
+                break
+            x_off = base_pt.x - shift_pt.x
+            y_off = base_pt.y - shift_pt.y
+            r_hexg = shapely.affinity.translate(s_hexg, xoff=x_off, yoff=y_off)
+            result_hexagons.append(r_hexg)
+        return result_hexagons
+
+    def _fitting(self, upper_polys, lower_polys):
+        result_uppers = [upper_polys.pop(0)]
+        result_lowers = []
+        while True:
+            if 1 <= len(lower_polys):
+                lowers = lower_polys.pop(0)
+                r_lowers = self._fit(result_uppers[-1], lowers)
+                result_lowers.append(r_lowers)
+            if 1 <= len(upper_polys):
+                uppers = upper_polys.pop(0)
+                r_uppers = self._fit(result_lowers[-1], uppers)
+                result_uppers.append(r_uppers)
+            if (len(upper_polys) == 0) & (len(lower_polys) == 0):
+                break
+        return result_uppers, result_lowers
     
     def _sort_hexagons(
         self, 
-        upper_polys: List[shapely.Polygon], 
-        lower_polys: List[shapely.Polygon]
+        upper_polys: List[List[shapely.Polygon]], 
+        lower_polys: List[List[shapely.Polygon]]
     ) -> SortedHexagons:
         """
         生成済みの六角形を左上から右下にかけて並び替え、idを割り当てる。\n
@@ -219,6 +295,7 @@ class Hexagons(object):
                 rows_idx(int): 行番号
                 cols_idx(int): 列番号
         """
+        upper_polys, lower_polys = self._fitting(upper_polys, lower_polys)
         hexagon_lst = []
         rows_idx = []
         cols_idx = []
@@ -244,8 +321,8 @@ class Hexagons(object):
     
     def _create_gdf(
         self, 
-        upper_polys: List[shapely.Polygon], 
-        lower_polys: List[shapely.Polygon]
+        upper_polys: List[List[shapely.Polygon]], 
+        lower_polys: List[List[shapely.Polygon]]
     ) -> gpd.GeoDataFrame:
         """
         生成済みの六角形を左上から右下にかけて並び替え、idを割り当てたGeoDataFr
@@ -276,17 +353,16 @@ class Hexagons(object):
         # 上記で作成した六角形を斜め下にずらし、フィットさせる
         lsbs_polys = self._lower_side_by_side(self.x_min, self.y_max, self.x_max)
         # 設定した範囲までデータをコピーし、ずらし続ける
-        _yoff = math.sqrt(self._square_metre / (1.5 * math.sqrt(3)))
-        yoff = (_yoff - _yoff / 100_000_000) * 3 * -1  # 少し重ねないとtouchesでFalseになるので
+        y_off = self.__move_down_y_off(sbs_polys[0])
         upper_polys = [sbs_polys]
         lower_polys = [lsbs_polys]
         while True:
-            shift_sbs_polys = self.__move_down(upper_polys[-1], yoff)
+            shift_sbs_polys = self.__move_down(upper_polys[-1], y_off)
             y_min = self.__calc_y_min(shift_sbs_polys[0])
             upper_polys.append(shift_sbs_polys)
             if y_min < self.y_min:
                 break
-            shift_lsbs_polys = self.__move_down(lower_polys[-1], yoff)
+            shift_lsbs_polys = self.__move_down(lower_polys[-1], y_off)
             y_min = self.__calc_y_min(shift_lsbs_polys[0])
             lower_polys.append(shift_lsbs_polys)
             if y_min < self.y_min:
@@ -318,7 +394,8 @@ def regular_hexagon(
 def regular_hexagon_gdf(
     hectare: float, 
     geometry: 
-        shapely.LineString 
+        shapely.MultiPoint
+        | shapely.LineString
         | shapely.MultiLineString 
         | shapely.MultiPolygon 
         | shapely.Polygon, 
@@ -340,3 +417,9 @@ def regular_hexagon_gdf(
     hxgs = Hexagons(hectare, *box)
     gdf = hxgs.create_hexagons_gdf
     return gdf
+
+
+
+
+
+
