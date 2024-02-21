4be43588fe3ce4077a23a2f487bc2a18a10d37bd
shingo405nagano <makishin.nagano@gmail.com>
2024-02-21 23:01:09 +0900

    fix

--- apps/processing_raster.py
+++ apps/processing_raster.py
@@ -5,9 +5,7 @@ from typing import Tuple
 
 import cv2
 import geopandas as gpd
-from matplotlib import pyplot as plt
 import numpy as np
-import pandas as pd
 import rasterio
 import shapely
 
@@ -17,6 +15,7 @@ from disassembly import geom_disassembly
 class RasterInfos(object):
     """Rasterの情報を取得する"""
     def __init__(self, ds: rasterio.io.DatasetReader):
+        self.bands = len(ds.indexes)
         self.epsg = ds.crs.to_epsg()
         bounds = ds.bounds
         self.x_min = bounds.left
@@ -25,13 +24,13 @@ class RasterInfos(object):
         self.y_max = bounds.top
         self.raster_rows = ds.width
         self.raster_cols = ds.height
-        self.raster_x_len_m = self.x_max - self.x_min
-        self.raster_y_len_m = self.y_max - self.y_min
-        self.px_x_len_m = self.raster_x_len_m / self.raster_rows
-        self.px_y_len_m = self.raster_y_len_m / self.raster_cols
         self.boundary_poly = self.__poly
         self.boundary_coords = self.__poly_coords
         self.epsg_utm = self.__estimate_utm_epsg
+        self.raster_width_m = self.__raster_width_m
+        self.raster_height_m = self.__raster_height_m
+        self.px_width_m = self.__raster_px_width_m
+        self.px_height_m = self.__raster_px_height_m
 
     @property
     def __poly(self) -> shapely.Polygon:
@@ -56,6 +55,59 @@ class RasterInfos(object):
             gdf = gdf.to_crs(crs='EPSG:4326')
         gdf_utm = gdf.to_crs(gdf.estimate_utm_crs())
         return gdf_utm.crs.to_epsg()
+    
+    @property
+    def __upper_left_point_utm(self):
+        coords_lst = self.__poly_coords
+        upper_left_coords = coords_lst[0]
+        upper_left_point = transform(
+            lon=upper_left_coords[0], 
+            lat=upper_left_coords[1], 
+            in_epsg=self.epsg, out_epsg=self.epsg_utm
+        )
+        return upper_left_point.points[0]
+
+    @property
+    def __upper_right_point_utm(self):
+        coords_lst = self.__poly_coords
+        upper_right_coords = coords_lst[1]
+        upper_right_point = transform(
+            lon=upper_right_coords[0], 
+            lat=upper_right_coords[1], 
+            in_epsg=self.epsg, out_epsg=self.epsg_utm
+        )
+        return upper_right_point.points[0]
+    
+    @property
+    def __lower_right_point_utm(self):
+        coords_lst = self.__poly_coords
+        lower_right_coords = coords_lst[2]
+        lower_right_point = transform(
+            lon=lower_right_coords[0], 
+            lat=lower_right_coords[1], 
+            in_epsg=self.epsg, out_epsg=self.epsg_utm
+        )
+        return lower_right_point.points[0]
+
+    @property
+    def __raster_width_m(self):
+        left = self.__upper_left_point_utm
+        right = self.__upper_right_point_utm
+        return abs(right.x - left.x)
+    
+    @property
+    def __raster_height_m(self):
+        top = self.__upper_right_point_utm
+        bottom = self.__lower_right_point_utm
+        return abs(top.y - bottom.y)
+    
+    @property
+    def __raster_px_width_m(self):
+        return self.raster_height_m / self.raster_rows
+    
+    @property
+    def __raster_px_height_m(self):
+        return self.raster_width_m / self.raster_cols
 
     @property
     def dictionary(self):
